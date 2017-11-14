a15c80dc2c1c9bfe4a7825ad4e40e24b7aed596e
marsor <369135912@qq.com>
2017-11-14 16:02:34 +0800

    首页幻灯片

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -24,7 +24,7 @@
       </div>
     </div>
     <div class="index-right">
-      <slide-show :slides="slides"></slide-show>
+      <slide-show :slides="slides" :inv="invTime"></slide-show>
       <div class="index-board-list">
         <div class="index-board-item" v-for="(item,index) in boardList"
              :class="[{'line-last':index%2!==0},'index-board-'+item.id]">
@@ -42,7 +42,12 @@
 </template>
 
 <script>
+  import slideShow from '../components/slideShow.vue'
+
   export default {
+    components: {
+      slideShow
+    },
     created: function () {
       this.$http.get('http://127.0.0.1:8080/getNewsList')
         .then((res) => {
@@ -54,6 +59,29 @@
     },
     data() {
       return {
+        invTime: 2000,
+        slides: [
+          {
+            src: require('../assets/slideShow/pic1.jpg'),
+            title: 'xxx1',
+            href: 'detail/analysis'
+          },
+          {
+            src: require('../assets/slideShow/pic2.jpg'),
+            title: 'xxx2',
+            href: 'detail/count'
+          },
+          {
+            src: require('../assets/slideShow/pic3.jpg'),
+            title: 'xxx3',
+            href: 'http://xxx.xxx.com'
+          },
+          {
+            src: require('../assets/slideShow/pic4.jpg'),
+            title: 'xxx4',
+            href: 'detail/forecast'
+          }
+        ],
         boardList: [
           {
             title: '开放产品',
